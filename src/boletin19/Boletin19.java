0d49fa7cdf5517511bacfa4c4caf4c61656c93fc
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2015-02-06 12:26:25 +0100

    still in progress

--- src/boletin19/Boletin19.java
+++ src/boletin19/Boletin19.java
@@ -1,11 +1,41 @@
-
 package boletin19;
-
 
 public class Boletin19 {
 
     public static void main(String[] args) {
-        
+        Buzon obx = new Buzon();
+       
+        int op;
+        do {
+            op = Buzon.menuSelect();
+            switch (op) {
+                case 1:
+                    obx.numeroDeCorreos();
+                    break;
+                case 2:
+                    obx.engade(Correo e);
+                    break;
+                case 3:
+                    //obx.porLer();
+                    break;
+                case 4:
+                    obx.amosaPrimerNonLeido();
+                    break;
+                case 5:
+                    //obx.amosa(k);
+                    break;
+                case 6:
+                    //obx.elimina(k);
+                    break;
+                case 7:
+                   if (op == 6 && op == 0) {
+                    System.exit(0); 
+                   }
+                    break;
+            }
+
+        } while (op != 7 && op !=0);
+
     }
-    
+
 }
--- src/boletin19/Buzon.java
+++ src/boletin19/Buzon.java
@@ -1,19 +1,30 @@
 
 package boletin19;
 
+import javax.swing.JOptionPane;
+
 public class Buzon {
   Correo mail=new Correo();
     
     public int numeroDeCorreos(){
-        int numail=0;
-        return numail;
-    }
-public void engade(Correo e){
-    
+        int numail;
+        for(numail=0;numail<mail.eMail.size();numail++)
+        if(numail==mail.eMail.size())
+            JOptionPane.showMessageDialog(null, " "+numail);
+      return numail;
+      }
+public void engade(Correo c){
+     String auxCorr;
+        boolean auxleid;
+        int pos = Integer.parseInt(JOptionPane.showInputDialog("introduce a posición na que queres engadir\n(posicións permitidas :(0,"  + "))"));
+        auxCorr = mail.getContido();
+        
+        auxleid = mail.isLeido();
+        mail.eMail.add(pos, new Correo(auxCorr, auxleid));
 }
 public boolean porLer(){
-    boolean sinler=true;
-    return sinler;
+    mail.leido=true;
+    return mail.leido;
 }
 public String amosaPrimerNonLeido(){
     String primNonLeido="";
@@ -26,4 +37,10 @@ public String amosa(int k){
 public void elimina(int k){
     
 }
+public static int menuSelect() {
+        int opcion = JOptionPane.showOptionDialog(null, "Selecciona unha opción", "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Numero de correos", "engade", "porLer", "amosa todo","amosa pos:", "elimina", "exit"}, "Sair do programa");
+        return opcion + 1;
+        
+    }
 }
+
